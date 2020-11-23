6ea7ca521552c2ce296ba3020c37acff6a043b6f
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-11-23 17:56:24 +0530

    UC4 : Stake Calculation for Month

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,10 +1,12 @@
 import java.util.Random;  
+import java.util.*;  
 public class GamblingSimulation {
 	public static final int START_STAKE = 100;
 	public static final int BET = 1;
 	public static final float STAKE_VALUE = 0.5f;
 	public static void main(String[] args) {
 		stakeCalculationForEachDay();
+		
 	}
         public static int gamble() {
 		Random random = new Random();
@@ -28,6 +30,7 @@ public class GamblingSimulation {
 	}
 	public static void stakeCalculationForEachDay(){
 		int[] stakeForEachDay= new int[20];
+		Map MonthlyStakemap=new HashMap();
 		for(int i=0;i<20;i++){
 			int dailyAmt = stakeCalculation();
 			stakeForEachDay[i] = dailyAmt;
@@ -35,5 +38,16 @@ public class GamblingSimulation {
 		for(int i=0;i<20;i++){
 			System.out.print(stakeForEachDay[i] +" ");
 		}
-	}
+		for(int i=0;i<20;i++){
+			int dailyAmt = stakeForEachDay[i];
+			MonthlyStakemap.put(i, dailyAmt);
+		}
+		Iterator it = MonthlyStakemap.entrySet().iterator();
+		System.out.println("");
+		while(it.hasNext()){  
+			Map.Entry pair = (Map.Entry)it.next();
+			System.out.println(" Day " + pair.getKey() + "  " + pair.getValue());
+			it.remove();  
+		}  
+	}		
 }
