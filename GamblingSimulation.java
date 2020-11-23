7a3210f60988a30068c16bb536ae34f1c03f6c34
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-11-23 17:36:18 +0530

    UC4 : Stake Calculation for 20 days

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -2,32 +2,38 @@ import java.util.Random;
 public class GamblingSimulation {
 	public static final int START_STAKE = 100;
 	public static final int BET = 1;
+	public static final float STAKE_VALUE = 0.5f;
 	public static void main(String[] args) {
-		stakeCalculation();
-    }
+		stakeCalculationForEachDay();
+	}
         public static int gamble() {
 		Random random = new Random();
 		int decision = random.nextInt(2);
 		return decision;
         }
-	public static void stakeCalculation() {
+	public static int stakeCalculation() {
 		int daily_stake = START_STAKE;
 		int daily_bet=BET;
-		while(daily_stake >= 50 && daily_stake <= 150) {
+		int winningamt = Math.round(daily_stake + daily_stake * STAKE_VALUE);
+		int lossingamt = Math.round(daily_stake - daily_stake * STAKE_VALUE);
+		while(daily_stake > lossingamt && daily_stake < winningamt) {
 			int outcome = gamble();
 			if(outcome == 1){
 				daily_stake+=daily_bet;
 			}
 			else
 				daily_stake-=daily_bet;
-			if(daily_stake == 50 ){
-				System.out.println("loose"+daily_stake);
-				break;
-			}
-			if(daily_stake == 150 ){
-				System.out.println("Win"+daily_stake);
-				break;
-			}
+		}
+		return daily_stake;
+	}
+	public static void stakeCalculationForEachDay(){
+		int[] stakeForEachDay= new int[20];
+		for(int i=0;i<20;i++){
+			int dailyAmt = stakeCalculation();
+			stakeForEachDay[i] = dailyAmt;
+		}
+		for(int i=0;i<20;i++){
+			System.out.print(stakeForEachDay[i] +" ");
 		}
 	}
 }
